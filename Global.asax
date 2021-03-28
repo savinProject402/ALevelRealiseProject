bf65561c6e7631b9a297178624b9c51d9d8c68a3
Олег Савін <oleh.savin@nure.ua>
2021-03-29 00:25:32 +0300

    Добавьте файлы проекта.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ALevelRealiseProject.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ALevelRealiseProject.Startup))]
+namespace ALevelRealiseProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
